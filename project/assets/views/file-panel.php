e559de2dc853f2b5ae4f9464564fe42f46830367
iarrieta90 <irati_arrieta90@hotmail.com>
2021-02-03 13:21:13 +0100

    file panel basic layout

---
+++ project/assets/views/file-panel.php
@@ -0,0 +1,41 @@
+    <article class="container_filepanel">
+        <section class="section">
+            <div class="container_path">
+                <p>Root>Documents>File 4</p>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container_tags">
+                <p class="tags_name"><span>Name</span></p>
+                <p class="tags_type"><span>Type</span></p>
+                <p class="tags_size"><span>Size</span></p>
+                <p class="tags_created"><span>Created</span></p>
+                <p class="tags_modified"><span>Modified</span></p>
+            </div>
+            <div class="container_files">
+                <div class="file_info__group">
+                    <div class="file_info__icon">
+                        <img  src="../img/word_icon.png" alt=""/>
+                    </div>
+                    <p class="file_info__name">Project.doc</p>
+                    <p class="file_info__type">doc</p>
+                    <p class="file_info__size">20KB</p>
+                    <p class="file_info__created">02-02-2021</p>
+                    <p class="file_info__modified">13:19</p>
+                </div>
+
+                <div class="file_info__group">
+                    <div class="file_info__icon">
+                        <img  src="../img/word_icon.png" alt=""/>
+                    </div>
+                    <p class="file_info__name">Project.doc</p>
+                    <p class="file_info__type">doc</p>
+                    <p class="file_info__size">20KB</p>
+                    <p class="file_info__created">02-02-2021</p>
+                    <p class="file_info__modified">13:19</p>
+                </div>
+            </div>
+        </section>
+    </article>
+
+
