2b7987d3886ef8db055d6a7402018c0ba14ee2f6
iarrieta90 <irati_arrieta90@hotmail.com>
2021-02-04 14:22:44 +0100

    navbar basic layout done, with button icons for the main functionalities

--- project/index.php
+++ project/index.php
@@ -7,17 +7,17 @@
   <link rel='stylesheet' href="assets/css/index.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
   <title>File Explorer</title>
 </head>
 <body>
-    <?php include('./assets/views/nav.php')?>
+    <?php include './assets/views/nav.php'?>
   <main>
     <?php 
     include 'assets/views/directorypanel.php'; 
-    include('./assets/views/file-panel.php');
-    include('./assets/views/file-view.php');
+    include './assets/views/file-panel.php';
+    include './assets/views/file-view.php';
     ?>
-    
   </main>
 <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>
 </body>
