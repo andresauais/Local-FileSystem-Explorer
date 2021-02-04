42927acb3588ee3942a115070c61ddd3524254bd
iarrieta90 <irati_arrieta90@hotmail.com>
2021-02-04 12:19:37 +0100

    all files and directories are displayed correctly on file panel, and their info is well formatted except for the image

--- project/assets/views/file-panel.php
+++ project/assets/views/file-panel.php
@@ -20,25 +20,27 @@
             </div>
             <div class="container_files">
                 
+                <!-- Loop iterates over filesArray and prints a file_info__group for each Fileinfo object-->
                 <?php 
-                    foreach($file as $k => $v):
+                    foreach($filesArray as $k => $v):
                 ?>
                     
                 <div class="file_info__group">
 
                     <div class="file_info__icon">
-                        <img src="./assets/img/word_icon.png" alt=""/>
+                        <img src="<?=$v->image?>" alt=""/>
                     </div>
-                    <p class="file_info__name"><?=$v?></p>
-                    <p class="file_info__type"><?=""?></p>
-                    <p class="file_info__size">20KB</p>
-                    <p class="file_info__created">02-02-2021</p>
-                    <p class="file_info__modified">13:19</p>
+                    <p class="file_info__name"><?=$v->name?></p>
+                    <p class="file_info__type"><?=$v->type?></p>
+                    <p class="file_info__size"><?=$v->size?></p>
+                    <p class="file_info__created"><?=$v->created?></p>
+                    <p class="file_info__modified"><?=$v->modified?></p>
                 </div>
 
                 <?php 
                 endforeach; 
                 ?>
+                <!-- End of foreach loop -->
 
             </div>
         </section>
