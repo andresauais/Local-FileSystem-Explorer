b2db67d69530ebec70419db6702d3b202c039555
Andres Auais <andresauais@gmail.com>
2021-02-05 08:55:17 +0000

    search for directories dynamically

--- project/assets/helpers/events.js
+++ project/assets/helpers/events.js
@@ -1,24 +1,33 @@
 $(".list-item").click(getDir);
+var dir = "../../../";
 
 function getDir(e){
   if($(e.target).hasClass('bx-right-arrow')){
-    console.log($(e.currentTarget).find("p").text());
     $(e.target).removeClass('bx-right-arrow');
     $(e.target).addClass('bx-down-arrow');
+    $($(e.target).next()).children().removeClass("bxs-folder");
+    $($(e.target).next()).children().addClass("bxs-folder-open");
     $.ajax({
       type:'POST',
       url: './assets/helpers/getDir.php',
-      data: {'name': $(e.currentTarget).find("p").text()}}).done(//cogerlo dinamico
+      data: {'name': $(e.currentTarget).find("p").text(),
+             'path': dir + $(e.currentTarget).find("p").text()},
+    }).done(
         function(data){
           $($(e.currentTarget).next()).append(data);
           $(".list-item").off();
           $(".list-item").click(getDir);
+          dir = dir + $(e.currentTarget).find("p").text() + '/';
       })
   }
   else if($(e.target).hasClass('bx-down-arrow')){
     $(e.target).removeClass('bx-down-arrow');
     $(e.target).addClass('bx-right-arrow');
+    $($(e.target).next()).children().removeClass("bxs-folder-open");
+    $($(e.target).next()).children().addClass("bxs-folder");
     $($(e.currentTarget).next()).children().remove();
+    dir = dir.split(($(e.currentTarget).find("p").text()));
+    dir = dir[0];
   }
 }
 //FIX this
