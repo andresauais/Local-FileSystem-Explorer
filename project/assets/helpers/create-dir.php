9725d485e613db5a33692feba115c2fa00f74a09
Andres Auais <andresauais@gmail.com>
2021-02-07 11:47:03 +0000

    creating directory and staying on the same page

--- project/assets/helpers/create-dir.php
+++ project/assets/helpers/create-dir.php
@@ -1,25 +1,25 @@
 <?php
 
 //validation to check if submit button was pressed and directory field is not empty
-if(isset($_POST["submit"]) && !empty($_POST["directory"])) {
-
+if(isset($_POST["submit"], $_POST['dir']) && !empty($_POST["directory"])) {
+    $dir=$_POST['dir'];
     //get the directory name from the form
     $directoryName = $_POST["directory"];
     //create the path for the new directory
-    $target_dir = dirname(__FILE__).'/../../../root/folder 1/'.$directoryName;
+    $target_dir = dirname(__FILE__).'/../../../'. $dir . '/'.$directoryName;
     //make directory in specified path
     mkdir($target_dir);
 
     //return to the index page
-    $url = "../../";
+    $url = "../../index.php?dir=" . $dir;
     header ('Location:' . $url);
     exit();
 
 //if the directory field is empty
 } else if (empty($_POST["directory"]) ){
-
+    $dir=$_POST['dir'];
     //redirect to the index page without changes
-    $url = "../../";
+    $url = "../../index.php?dir=" . $dir;
     header ('Location:' . $url);
     exit();
 }
--- project/assets/views/modal-create-dir.php
+++ project/assets/views/modal-create-dir.php
@@ -8,6 +8,7 @@
         <form class="modal_form__group" method="POST" action="./assets/helpers/create-dir.php">
             <label for="directory">Directory name</label>
             <input class="input_text" type="text" name="directory"/>
+            <input type='hidden' name='dir' value='<?php echo $_GET['dir'];?>'/>
             <input class="btn_submit" type="submit" name="submit" value="Add"/>
         </form>
     </section>
