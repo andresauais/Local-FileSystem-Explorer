a69b77901358cc72559d871812b1c304bbaaf8d4
Andres Auais <andresauais@gmail.com>
2021-02-08 15:59:30 +0000

    update

--- project/assets/views/nav.php
+++ project/assets/views/nav.php
@@ -12,7 +12,6 @@
             </div>
             <div class="navbar_actions__right">
             <form class="form_search" method="POST" action="./assets/helpers/search.php">
-<<<<<<< HEAD
                 <input class="input_text" type="text" name="search-file"/>
                 <input type='hidden' name='dir' value='<?php echo (isset($_GET['dir']) ? $_GET['dir'] : "root");?>'/>
                 <button type="submit" class="btn_search btn_transparent btn_nav">
@@ -24,18 +23,6 @@
             <button class="btn_rename btn_transparent btn_nav"><i class='bx bx-rename'></i></button>
             <button class="btn_move btn_transparent btn_nav"><i class='bx bx-move-horizontal'></i></button>
             <button class="btn_upload btn_transparent btn_nav"><i class='bx bx-upload' ></i></button>
-=======
-                    <input class="input_text" type="text" name="search-file" />
-                    <input type='hidden' name='dir' value='<?php echo (isset($_GET['dir']) ? $_GET['dir'] : "root"); ?>' />
-                    <button type="submit" class="btn_search btn_transparent btn_nav">
-                        <i class='bx bx-search'></i>
-                    </button>
-                </form>
-                <button class="btn_remove btn_transparent btn_nav"><i class='bx bx-trash'></i></button>
-                <button class="btn_remove btn_transparent btn_nav"><i class='bx bx-rename'></i></button>
-                <button class="btn_remove btn_transparent btn_nav"><i class='bx bx-move-horizontal'></i></button>
-            </div>
->>>>>>> 284dfb7f4a6bc2a511b20ee765ad407f752cbfdc
         </div>
     </nav>
 </header>
